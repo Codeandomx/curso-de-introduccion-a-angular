363f19fa8904fc265304e82855945fc0ae152cd2
Codeandomx <admin@codeando.org>
2018-01-22 23:36:18 -0600

    Ejemplo de interpolacion, property y event binding Angular.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	sitiosInteres: any = [];
+	sitiosInteres: any[];
+  formTitle: string;
+  listTitle: string;
+  btnText: string;
+  list: any[];
+  site: iSite;
 
     constructor() { }
 
@@ -18,6 +23,19 @@ export class HomeComponent implements OnInit {
             { name: 'Twitter', url: 'https://twitter.com/codeando_org' },
             { name: 'Facebook', url: 'https://facebook.com/codeando.org' }
         ];
+        this.formTitle = "Agrega un item a la lista";
+        this.listTitle = "Lista de items";
+        this.btnText = "Agregar item";
+        this.site = { name: 'Blog Codeando', url: 'http://blog.codeando.org' };
     }
 
+    setSite(site: iSite) {
+      this.site = site;
+    }
+
+}
+
+interface iSite{
+  name: string,
+  url: string
 }
