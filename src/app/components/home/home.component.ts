5d6b0648651fb1fa69cc89bc4fa59179f2b3ef6c
Codeandomx <admin@codeando.org>
2018-01-23 21:17:54 -0600

    Agregamos comentarios al componente

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,40 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+// Decorador
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
+// clase del componente
+export class HomeComponent implements OnInit
+{
+  // propiedades
 	sitiosInteres: any[];
   formTitle: string;
   listTitle: string;
   btnText: string;
-  list: any[];
   site: iSite;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() {
-        this.sitiosInteres = [
-            { name: 'Blog Codeando', url: 'http://blog.codeando.org' },
-            { name: 'Github', url: 'https://github.com/codeandomx' },
-            { name: 'Twitter', url: 'https://twitter.com/codeando_org' },
-            { name: 'Facebook', url: 'https://facebook.com/codeando.org' }
-        ];
-        this.formTitle = "Agrega un item a la lista";
-        this.listTitle = "Lista de items";
-        this.btnText = "Agregar item";
-        this.site = { name: 'Blog Codeando', url: 'http://blog.codeando.org' };
-    }
+  // Inicializamos las propiedades
+  ngOnInit() {
+    this.sitiosInteres = [
+      { name: 'Blog Codeando', url: 'http://blog.codeando.org' },
+      { name: 'Github', url: 'https://github.com/codeandomx' },
+      { name: 'Twitter', url: 'https://twitter.com/codeando_org' },
+      { name: 'Facebook', url: 'https://facebook.com/codeando.org' }
+    ];
+    this.formTitle = "Agrega un item a la lista";
+    this.listTitle = "Lista de items";
+    this.btnText = "Agregar item";
+    this.site = { name: 'Blog Codeando', url: 'http://blog.codeando.org' };
+  }
 
-    setSite(site: iSite) {
-      this.site = site;
-    }
-
+  // Actualizamos el sitio de interes principal
+  setSite(site: iSite) {
+    this.site = site;
+  }
 }
 
+// Interfaz para sitios
 interface iSite{
   name: string,
   url: string
